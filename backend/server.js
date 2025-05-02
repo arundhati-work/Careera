79fcead9df63837b657c00a1ab3d8412c8980b0c
Arundhati Bandopadhyaya <arundhatib.work@gmail.com>
2025-05-01 22:56:35 -0400

    user model

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27018/careera", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}). then(() => {
+    console.log("MongoDB connected")
+})
+.catch(err => console.error(err));
+
+app.listen(8080, () => {
+    console.log("App listening on PORT 8080");
+})
